deca71ec04bf2cb8e9c2e1a8849309df9e62ba85
Alice2506 <ralko84@mail.ru>
2022-09-25 15:31:53 +0300

    example006 base added

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;
+int b = 2;
+int d = 8;
+int c = 4;
+int e = 6;
+
+int max = 0;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(d > max) max = d;
+if(c > max) max = c;
+if(e > max) max = e;
+
+Console.Write(max);
