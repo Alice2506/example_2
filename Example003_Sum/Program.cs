c2c59cbebdeb369674488b28a4912a09573dafe8
Alice2506 <ralko84@mail.ru>
2022-09-25 11:27:45 +0300

    example003 Sum added

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = 9;
+int numberB = 57;
+int result = numberA + numberB;
+Console.WriteLine(result);
